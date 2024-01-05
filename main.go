e952e83076ed151483f3935ce1ec888b45809c22
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2024-01-05 12:08:31 -0300

    endpoint creat user

--- main.go
+++ main.go
@@ -4,11 +4,20 @@ import (
 	"log"
 
 	"github.com/andrealopezpalomeque/fiber-api/database"
+	"github.com/andrealopezpalomeque/fiber-api/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to the API!")
+}
+
+func setupRoutes(app *fiber.App) {
+	//welcome endpoint
+	app.Get("/api", welcome)
+
+	//USER ENDPOINTS
+	app.Post("/api/users", routes.CreateUser)
 }
 
 func main() {
@@ -17,8 +26,7 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/api", welcome)
-
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
---
+++ routes/user.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"github.com/andrealopezpalomeque/fiber-api/database"
+	"github.com/andrealopezpalomeque/fiber-api/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+
+type User struct {
+	//? This is not the model, more like a serializer
+	ID uint `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+}
+
+func CreateResponseUser(user models.User) User {
+	return User{ID: user.ID, FirstName: user.FirstName, LastName: user.LastName}
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Create(&user)
+	responseUser := CreateResponseUser(user)
+
+	return c.Status(200).JSON(responseUser)
+	
+}
